01b34fba3bc7d8dfd2d16ce195fe36641be79ee3
Oozyx <hendrik.oosenbrug@gmail.com>
2022-12-12 14:25:25 -0500

    Scaffold create game

--- ts-client/tictactoe.tictactoe/registry.ts
+++ ts-client/tictactoe.tictactoe/registry.ts
@@ -1,6 +1,8 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
+import { MsgCreateGame } from "./types/tictactoe/tictactoe/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
+    ["/tictactoe.tictactoe.MsgCreateGame", MsgCreateGame],
     
 ];
 
--- ts-client/tictactoe.tictactoe/rest.ts
+++ ts-client/tictactoe.tictactoe/rest.ts
@@ -20,6 +20,8 @@ export interface RpcStatus {
   details?: ProtobufAny[];
 }
 
+export type TictactoeMsgCreateGameResponse = object;
+
 /**
  * Params defines the parameters for the module.
  */
